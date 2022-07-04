dd885dd1e83c58625cec61ce39945f4d52108b8f
unknown <suryaagus071@gmail.com>
2022-07-04 15:09:11 +0700

    a

---
+++ kemampuan-dasar-1/kasus/file-ren.ps1
@@ -0,0 +1,3 @@
+@ECHO OFF
+get-command "java"
+PAUSE
---
+++ kemampuan-dasar-1/latihan/powershellscripts/coba.ps1
@@ -0,0 +1,5 @@
+﻿Get-WmiObject -Class .java –ComputerName localhost |
+
+Select-Object -Property CSName,@{n=”Last Booted”;
+
+e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}}
